5abd7881058db8dff01414b25cc403b4b99b95a9
u <u@abc.com>
2020-06-02 09:57:19 +0800

    2020.1 java

--- src/main/kotlin/zjhmale/rainbow/RainbowHighliter.kt
+++ src/main/kotlin/zjhmale/rainbow/RainbowHighliter.kt
@@ -9,7 +9,6 @@ import com.intellij.psi.JavaTokenType
 import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.source.tree.LeafPsiElement
 import zjhmale.rainbow.encode.HashFace
-import zjhmale.rainbow.setting.RainbowSettings
 import java.awt.Color
 import java.awt.Font
 import java.util.regex.Pattern
@@ -20,6 +19,13 @@ import java.util.regex.Pattern
 class RainbowHighliter : Annotator {
     private val delimitersList = arrayOf("(", ")", "{", "}", "[", "]")
     //https://github.com/JetBrains/kotlin/blob/master/compiler/frontend/src/org/jetbrains/kotlin/kdoc/lexer/KDocTokens.java
+    private val groovyDocTokens = arrayOf(
+            "GDOC_COMMENT_START", "GDOC_COMMENT_END", "GDOC_COMMENT_DATA",
+            "GDOC_TAG_NAME", "GDOC_WHITESPACE", "GDOC_TAG_VALUE_TOKEN",
+            "GDOC_TAG_VALUE_LPAREN", "GDOC_TAG_VALUE_RPAREN", "GDOC_TAG_VALUE_GT",
+            "GDOC_TAG_VALUE_LT", "GDOC_INLINE_TAG_END", "DOC_INLINE_TAG_START",
+            "GDOC_TAG_VALUE_COMMA", "GDOC_TAG_VALUE_SHARP_TOKEN", "GDOC_LEADING_ASTERISKS",
+            "DOC_COMMENT_BAD_CHARACTER")    
     private val kotlinDocTokens = arrayOf(
             "KDOC_START", "KDOC_END", "KDOC_LEADING_ASTERISK",
             "KDOC_TEXT", "KDOC_TAG_NAME", "KDOC_MARKDOWN_LINK",
@@ -32,13 +38,7 @@ class RainbowHighliter : Annotator {
             "DOC_TAG_VALUE_DOT", "DOC_TAG_VALUE_COMMA", "DOC_TAG_VALUE_LPAREN",
             "DOC_TAG_VALUE_RPAREN", "DOC_TAG_VALUE_SHARP_TOKEN")
     //https://github.com/JetBrains/intellij-community/blob/master/plugins/groovy/groovy-psi/src/org/jetbrains/plugins/groovy/lang/groovydoc/lexer/GroovyDocTokenTypes.java
-    private val groovyDocTokens = arrayOf(
-            "GDOC_COMMENT_START", "GDOC_COMMENT_END", "GDOC_COMMENT_DATA",
-            "GDOC_TAG_NAME", "GDOC_WHITESPACE", "GDOC_TAG_VALUE_TOKEN",
-            "GDOC_TAG_VALUE_LPAREN", "GDOC_TAG_VALUE_RPAREN", "GDOC_TAG_VALUE_GT",
-            "GDOC_TAG_VALUE_LT", "GDOC_INLINE_TAG_END", "DOC_INLINE_TAG_START",
-            "GDOC_TAG_VALUE_COMMA", "GDOC_TAG_VALUE_SHARP_TOKEN", "GDOC_LEADING_ASTERISKS",
-            "DOC_COMMENT_BAD_CHARACTER")
+
     private val haskellMultilineCommentPattern = Pattern.compile(
             "\\{-.*?-\\}"
     )
@@ -127,7 +127,7 @@ class RainbowHighliter : Annotator {
     val isNormalMultilineComment = { element: PsiElement -> isMultilineComment(element, normalMultilineCommentPattern) }
 
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
-        val settings = RainbowSettings.getInstance()
+//        val settings = RainbowSettings.getInstance()
 
         val scheme = EditorColorsManager.getInstance().globalScheme
         val backgroundColor = scheme.defaultBackground
@@ -137,7 +137,7 @@ class RainbowHighliter : Annotator {
         if (element is LeafPsiElement
                 && delimitersList.contains(element.text)
                 && languageID != "Clojure"
-                && settings.isRainbowDelimiter
+//                && settings.isRainbowDelimiter
                 && !isString(element)
                 && !isHaskellMultilineComment(element)
                 && !isNormalMultilineComment(element)) {
@@ -146,73 +146,78 @@ class RainbowHighliter : Annotator {
             holder.createInfoAnnotation(element as PsiElement, null).setEnforcedTextAttributes(attrs)
         }
 
-        if (element is LeafPsiElement && settings.isRainbowIdentifier) {
+//        if (element is LeafPsiElement && settings.isRainbowIdentifier) {
+        if (element is LeafPsiElement) {
             val type = element.elementType
             val t = element.getNode().text
 
             val javaPredicate = languageID == "JAVA"
-                    && type !== JavaTokenType.C_STYLE_COMMENT
-                    && type !== JavaTokenType.END_OF_LINE_COMMENT
-                    && !JavaDocTokenType.ALL_JAVADOC_TOKENS.contains(type)
-            val kotlinPredicate = languageID == "kotlin"
-                    && !t.startsWith("//")
-                    && !(t.startsWith("/*") && t.endsWith("*/"))
-                    && !kotlinDocTokens.contains(type.toString())
-            val clojurePredicate = languageID == "Clojure"
-                    && !t.startsWith(";")
-                    && !t.startsWith("#_")
-                    && !t.startsWith("#(")
-                    && !t.startsWith("#{")
-                    && !ignoreNextFormParent(element)
-            val pythonPredicate = languageID == "Python" && !t.startsWith("#")
-            val rubyPredicate = languageID == "ruby" && !t.startsWith("#")
-            val elixirPredicate = languageID == "Elixir" && !t.startsWith("#")
-            //for Haskell and Agda
-            val haskellLikePredicate = (languageID == "Haskell" || languageID == "Agda")
-                    && !t.startsWith("--")
-                    && !isHaskellMultilineComment(element)
-            val rustPredicate = languageID == "RUST"
-                    && !t.startsWith("//")
-                    && !(t.startsWith("/*") && t.endsWith("*/"))
-            val jsPredicate = languageID == "JavaScript"
-                    && !t.startsWith("//")
-                    && !isNormalMultilineComment(element)
-            val erlangPredicate = languageID == "Erlang" && !t.startsWith("%")
-            val scalaPredicate = languageID == "Scala"
-                    && !t.startsWith("//")
-                    && !(t.startsWith("/*") && t.endsWith("*/"))
-                    && !scalaDocTokens.contains(type.toString())
-            val goPredicate = languageID == "go"
-                    && !t.startsWith("//")
-                    && !(t.startsWith("/*") && t.endsWith("*/"))
-            val groovyPredicate = languageID == "Groovy"
-                    && !t.startsWith("//")
-                    && !(t.startsWith("/*") && t.endsWith("*/"))
-                    && !groovyDocTokens.contains(type.toString())
-            val cppPredicate = languageID == "ObjectiveC"
-                    && !t.startsWith("//")
-                    && !isNormalMultilineComment(element)
-            val phpPredicate = languageID == "PHP"
-                    && !t.startsWith("//")
-                    && !isNormalMultilineComment(element)
+//                    && type !== JavaTokenType.C_STYLE_COMMENT
+//                    && type !== JavaTokenType.END_OF_LINE_COMMENT
+//                    && !JavaDocTokenType.ALL_JAVADOC_TOKENS.contains(type)
+            
+//            val groovyPredicate = languageID == "Groovy"
+//                    && !t.startsWith("//")
+//                    && !(t.startsWith("/*") && t.endsWith("*/"))
+//                    && !groovyDocTokens.contains(type.toString())            
+            
+//            val kotlinPredicate = languageID == "kotlin"
+//                    && !t.startsWith("//")
+//                    && !(t.startsWith("/*") && t.endsWith("*/"))
+//                    && !kotlinDocTokens.contains(type.toString())
+//            val clojurePredicate = languageID == "Clojure"
+//                    && !t.startsWith(";")
+//                    && !t.startsWith("#_")
+//                    && !t.startsWith("#(")
+//                    && !t.startsWith("#{")
+//                    && !ignoreNextFormParent(element)
+//            val pythonPredicate = languageID == "Python" && !t.startsWith("#")
+//            val rubyPredicate = languageID == "ruby" && !t.startsWith("#")
+//            val elixirPredicate = languageID == "Elixir" && !t.startsWith("#")
+//            //for Haskell and Agda
+//            val haskellLikePredicate = (languageID == "Haskell" || languageID == "Agda")
+//                    && !t.startsWith("--")
+//                    && !isHaskellMultilineComment(element)
+//            val rustPredicate = languageID == "RUST"
+//                    && !t.startsWith("//")
+//                    && !(t.startsWith("/*") && t.endsWith("*/"))
+//            val jsPredicate = languageID == "JavaScript"
+//                    && !t.startsWith("//")
+//                    && !isNormalMultilineComment(element)
+//            val erlangPredicate = languageID == "Erlang" && !t.startsWith("%")
+//            val scalaPredicate = languageID == "Scala"
+//                    && !t.startsWith("//")
+//                    && !(t.startsWith("/*") && t.endsWith("*/"))
+//                    && !scalaDocTokens.contains(type.toString())
+//            val goPredicate = languageID == "go"
+//                    && !t.startsWith("//")
+//                    && !(t.startsWith("/*") && t.endsWith("*/"))
+
+//            val cppPredicate = languageID == "ObjectiveC"
+//                    && !t.startsWith("//")
+//                    && !isNormalMultilineComment(element)
+//            val phpPredicate = languageID == "PHP"
+//                    && !t.startsWith("//")
+//                    && !isNormalMultilineComment(element)
 
             val isParentheses = delimitersList.contains(t)
 
             if ((javaPredicate
-                    || kotlinPredicate
-                    || clojurePredicate
-                    || pythonPredicate
-                    || rubyPredicate
-                    || haskellLikePredicate
-                    || rustPredicate
-                    || jsPredicate
-                    || erlangPredicate
-                    || scalaPredicate
-                    || goPredicate
-                    || groovyPredicate
-                    || elixirPredicate
-                    || cppPredicate
-                    || phpPredicate)
+//                    || groovyPredicate
+//                    || kotlinPredicate
+//                    || clojurePredicate
+//                    || pythonPredicate
+//                    || rubyPredicate
+//                    || haskellLikePredicate
+//                    || rustPredicate
+//                    || jsPredicate
+//                    || erlangPredicate
+//                    || scalaPredicate
+//                    || goPredicate
+//                    || elixirPredicate
+//                    || cppPredicate
+//                    || phpPredicate
+                            )
                     && !isParentheses
                     && !isString(element)) {
                 val attrs = getIdentifierAttributes(t, backgroundColor)
